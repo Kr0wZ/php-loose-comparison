0e25a30ee31c10436235df292805f57b0314646b
Kr0wZ <67439924+Kr0wZ@users.noreply.github.com>
2020-07-10 01:09:28 +0200

    example from php-type-juggling article

---
+++ article-example.php
@@ -0,0 +1,17 @@
+//This code comes from my article : https://kr0wz.github.io/fr/2020/07/0/php-type-juggling.html
+
+<?php
+
+if(isset($_GET['username']) && isset($_GET['password'])){
+	if($_GET['username'] == TRUE && $_GET['username'] == md5($_GET['password'])){
+		echo "Welcome MASTER !";
+	}
+	else {
+		echo "Sorry wrong credentials";
+	}
+}
+else {
+	echo "You must specify both username and password in URL ...";
+}
+
+?>
